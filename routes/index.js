dfa49d074f46399a10b82fe421343936f3ceaf86
pi1 <imdesigner2012@gmail.com>
2016-11-12 16:15:04 -0700

    Insert working

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,22 @@ router.get('/numbers', function(req, res) {
     res.render('numbers', { title: 'NUMBERS' });
 });
 
+router.get('/sensor_post', function(req, res) {
+    console.log("********************PREPROMISE");
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.insert({
+    "sensorid": 3,
+    "temp": Math.floor(Math.random()*120),
+    "humidity": Math.floor(Math.random()*120),
+    "test": true
+    }).then(function(){
+	console.log("PROMISE");
+        res.redirect('/test1');
+    });
+
+});
+
 /* GET 'test1' page */
 //Should not use 'usercollection' --> should make new collection after inital dev
 
@@ -21,13 +37,13 @@ router.get('/test1', function(req, res) {
     var collection = db.get('usercollection');
     var humidityArr;
     var tempArr;
-    console.log("======================", collection);
 
     collection.find({}, {}, function(e,results){
         var stringResults = JSON.stringify(results);
-    	
+    	console.log("****************************", stringResults);
+	console.log("----------------------------", e);
         res.render('index', {
-             'test1' : JSON.stringify(results),
+             'test1' : stringResults,
 	     'title' : 'RoboPlant!'
         });
     });
